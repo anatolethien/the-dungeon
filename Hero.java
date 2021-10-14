426a1d66bc80ba00cbf1e95654da6bafae8d3f7c
anatolethien <anatole.thien@ynov.com>
2021-10-15 00:06:13 +0200

    rm Hero.java from root

--- Hero.java
+++
@@ -1,8 +0,0 @@
-public class Hero {
-    // constructor
-    public Hero(String name) {
-        System.out.printf("A new hero named \"%s\" appeared!%n", name);
-    }
-    // data
-    public String name;
-}
